004deec03ed955cf359df57055788dacb04b22d8
shiladitya <97834321+aditya0116@users.noreply.github.com>
2022-05-03 00:14:19 +0530

    Update 1587.sql

--- 1587.sql
+++ 1587.sql
@@ -1,1 +1,4 @@
-select name , sum(amount) balance from users u join transactions t on u.account = t.account group by 1 having sum(amount) > 10000 ;
+with temp as (
+select name , sum(amount) balance from users u join transactions t on u.account = t.account group by 1 
+    )
+select * from temp where balance > 10000 ;
