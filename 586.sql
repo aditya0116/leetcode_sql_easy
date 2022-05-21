1dbd0586b22f6df2e15d7254b872c32e9b692c8a
shiladitya <97834321+aditya0116@users.noreply.github.com>
2022-05-22 00:37:40 +0530

    Update 586.sql

--- 586.sql
+++ 586.sql
@@ -1,1 +1,1 @@
-select customer_number from Orders where order_number = (select max(order_number) from Orders) ;
+select customer_number from (select customer_number,count(order_number) over(partition by customer_number) rn from  Orders)a order by rn desc limit 1 ;
